4050cc361532b5281d6823f52f0b9c4caa726112
Ashley Ruglys <ashley.ruglys@gmail.com>
2021-11-24 11:18:24 +0100

    Add a flag for spirv metadata

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder};
+use spirv_builder::{Capability, MetadataPrintout, SpirvBuilder, SpirvMetadata};
 use std::path::PathBuf;
 use std::str::FromStr;
 use structopt::StructOpt;
@@ -22,6 +22,20 @@ struct Opt {
     /// The directory to write the output shaders to. By default they're written to the parent of <path>.
     #[structopt(long)]
     output: Option<PathBuf>,
+    #[structopt(long, default_value = "none", parse(try_from_str = parse_spirv_metadata))]
+    spirv_metadata: SpirvMetadata,
+}
+
+fn parse_spirv_metadata(string: &str) -> anyhow::Result<SpirvMetadata> {
+    match string {
+        "none" => Ok(SpirvMetadata::None),
+        "full" => Ok(SpirvMetadata::Full),
+        "name-variables" => Ok(SpirvMetadata::NameVariables),
+        _ => Err(anyhow::anyhow!(
+            "Expecting one of none,full,name-variables: {}",
+            string
+        )),
+    }
 }
 
 fn parse_capability(string: &str) -> anyhow::Result<Capability> {
@@ -48,7 +62,8 @@ fn main() -> anyhow::Result<()> {
     let mut builder = SpirvBuilder::new(&opt.path, &opt.target)
         .print_metadata(MetadataPrintout::None)
         .multimodule(opt.multimodule)
-        .release(!opt.debug);
+        .release(!opt.debug)
+        .spirv_metadata(opt.spirv_metadata);
 
     for extension in &opt.extensions {
         builder = builder.extension(extension);
